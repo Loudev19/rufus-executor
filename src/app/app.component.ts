5b1420a37ac4dc7a83ef0b587270b85b9a055bb7
Maria <mb.lev17@gmail.com>
2020-11-15 15:38:09 -0500

    Styling python error messages and handled tabs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { ExecService } from "./exec.service";
 
 @Component({
@@ -9,15 +9,21 @@ import { ExecService } from "./exec.service";
 export class AppComponent {
   title = 'rufus-executor';
 
+  @ViewChild('r', { read: ElementRef, static: false }) r: ElementRef;
+
   response: String = "";
   code: String = "";
+  pythonError: boolean = false;
 
   constructor(private _exec: ExecService,) { }
 
   execute() {
+    console.log(this.code);
     this._exec.executeCode(this.code).subscribe(
       res => {
         this.response = res;
+        if (res.includes("PROGRAM FAILED: ")) this.pythonError = true;
+        else this.pythonError = false;
       },
       error => {
         console.log(error);
@@ -28,7 +34,16 @@ export class AppComponent {
   onTab(event) {
     if (event.key === 'Tab') {
       event.preventDefault();
-      this.code += "  ";
+      this.click("\t");
     }
+  }
+
+  click(e) {
+    var startPos = this.r.nativeElement.selectionStart;
+    this.r.nativeElement.focus();
+    this.r.nativeElement.value = this.r.nativeElement.value.substr(0, this.r.nativeElement.selectionStart) + e + this.r.nativeElement.value.substr(this.r.nativeElement.selectionStart, this.r.nativeElement.value.length);
+    this.code = this.r.nativeElement.value;
+    this.r.nativeElement.selectionStart = startPos;
+    this.r.nativeElement.focus();
   }
 }
